9fdd4a88443b51980ef23b5dd87e16a7e8b24c1c
Joseph August DeLeonardis <etdel651@josephs-macbook-pro.local>
2018-02-19 09:33:41 -0800

    Initial Commit

---
+++ Lab8/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab8
+//
+//  Created by Joseph DeLeonardis  on 2/19/18.
+//  Copyright © 2018 Joseph DeLeonardis . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
